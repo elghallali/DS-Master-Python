e131af8412e368639a05f2671919d8884d867aac
Yassine EL GHALLALI <35077623+elghallali@users.noreply.github.com>
2022-11-12 23:54:34 +0100

    Second update of equation2dgree.py

--- equation2dgree.py
+++ equation2dgree.py
@@ -14,19 +14,21 @@ c = float(input("Entrez le nombre c: "))
 delta = b**2 - 4 * a * c
 print("delta est: ", delta)
 if delta > 0:
-    x1= (-b + math.sqrt(delta)) / (2 * a)
-    x2= (-b - math.sqrt(delta)) / (2 * a)
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
     print("deux solutions réels x1 et x2 ")
     print("x1= ", x1)
-    print("x2= ", x2 )
+    print("x2= ", x2)
+    
 elif delta == 0:
     x = -b / (2 * a)
     print("une seule solution est:")
     print("x= ", x)
 
 else:
-    x1= str(-b / (2 * a)) + " + " + str(math.sqrt(-delta) / (2 * a)) + "i"
-    x2= str(-b / (2 * a)) + " - " + str(math.sqrt(-delta) / (2 * a)) + "i"
+    x1 = str(-b / (2 * a)) + " + " + str(math.sqrt(-delta) / (2 * a)) + "i"
+    x2 = str(-b / (2 * a)) + " - " + str(math.sqrt(-delta) / (2 * a)) + "i"
     print("deux solutions complexe")
     print("x1= ", x1)
+
     print("x2= ", x2)
