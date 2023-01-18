a6b6e30758e282f5c8d5fd20f588bc5fafc91b7e
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-01-18 13:43:07 +0100

    Update mysql_db.py

--- mysql_db.py
+++ mysql_db.py
@@ -1,4 +1,4 @@
-# pip install mysql.connector
+# pip install mysql-connector-python
 
 # import libraries
 import mysql.connector
@@ -10,17 +10,19 @@ try:
     user="root",
     password="20171987"
     )
-
+  print(conn)
   db = conn.cursor()
-  myDataBase = "mydatabas"
+  myDataBase = "test_master"
   db.execute("SHOW DATABASES")
   lst = db.fetchall()
   if (myDataBase,) not in lst:
-    db.execute("CREATE DATABASE {}".format(myDataBase))
+    db.execute(f"CREATE DATABASE {myDataBase}")
+    db.execute("CREATE TABLE etudiant (name VARCHAR(255), address VARCHAR(255))")
   print(lst)
-  db.execute("SELECT * FROM SAKILA.ACTOR")
+
+  db.execute("SELECT * FROM test_master.personne")
   lst1 = db.fetchall()
   df = pd.DataFrame(lst1)
   print(df)
 except mysql.connector.Error as err:
-  print("Something went wrong: {}".format(err))
+  print("Something went wrong: {err}")
